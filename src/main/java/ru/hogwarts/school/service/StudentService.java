b5cbb789cac8e9a7513d5649123cd4ac6007126d
LUM <lumlum177@gmail.com>
2023-01-21 16:02:17 +0300

    Коммит после выполнения заданий

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> findByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+    public Collection<Student> findByAgeBetween(int min, int max) {
+        return studentRepository.findStudentByAgeBetween(min, max);
+    }
+
+    public Collection<Student> findByFaculty(long faculty) {
+        return studentRepository.findStudentByFaculty_Id(faculty);
+    }
+}
